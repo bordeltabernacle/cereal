3db052d980f89a226ba27dbe71a9181f3d286331
Rob Phoenix <robertphoenix@gmail.com>
2016-02-18 17:11:37 +0000

    add cereal struct and switch_map function

--- lib/cereal.ex
+++ lib/cereal.ex
@@ -1,10 +1,21 @@
-defmodule Cereal do
+defmodule CerealStruct do
 
   defstruct [hostname: nil,
              serial: nil,
              version: nil,
              image: nil,
              model: nil]
+
+end
+
+defmodule Cereal do
+
+  @hostname_regex ~r/hostname\s(?<hostname>\w+)/
+  @model_regex ~r/(?<model>\w+-\w+-\w+)/
+  @sh_ver_serial_regex ~r/\w+:\s(?<serial>\w+)/
+  @sh_inv_serial_regex ~r/SN:\s(?<serial>\w+)/
+  @image_regex ~r/"flash:\/(?<image>\w+-\w+-\w+\.\d+-\d+\.\w+)/
+  @version_regex ~r/Version\s(?<version>\d{2}\.[\w\.)?(?]+)/
 
   def start_link(port \\ "/dev/ttyUSB0", baud \\ 9600) do
     {:ok, serial} = Serial.start_link
@@ -25,7 +36,7 @@ defmodule Cereal do
 
         listen(acc)
 
-    after 1_300 -> acc
+    after 2_000 -> acc
     end
   end
 
@@ -33,38 +44,42 @@ defmodule Cereal do
     start_link |> send_data(data <> "\r\n")
   end
 
+  def switch_map() do
+    switch = %CerealStruct{hostname: hostname(),
+            serial: serial(),
+            version: version(),
+            image: image(),
+            model: model()}
+    IO.puts "\nHostname:\t\t\t\t#{switch.hostname}\nSerial Number:\t#{switch.serial}\nVersion:\t\t\t\t#{switch.version}\nImage:\t\t\t\t\t#{switch.image}\nModel:\t\t\t\t\t#{switch.model}\n"
+  end
+
   def hostname() do
     speak "sh run | inc hostname"
     IO.puts "Retrieving switch hostname..."
     result = listen()
     %{"hostname" => hostname} = Regex.named_captures(
-      ~r/hostname\s(?<hostname>\w+)/, result)
-    cond do
-      hostname == nil ->
-        {:error, "Unable to retrieve hostname"}
-      true ->
-        {:ok, hostname}
-    end
+      @hostname_regex, result)
+    hostname
   end
 
   def serial() do
     speak "sh inv | inc SN:"
     IO.puts "Retrieving serial number..."
     %{"serial" => serial_1} = Regex.named_captures(
-      ~r/SN:\s(?<serial>\w+)/, listen())
+      @sh_inv_serial_regex, listen())
 
     speak "sh ver | inc System serial"
     IO.puts "Retrieving serial number..."
     %{"serial" => serial_2} = Regex.named_captures(
-      ~r/\w+:\s(?<serial>\w+)/, listen())
+      @sh_ver_serial_regex, listen())
 
     IO.puts "Checking serial number..."
 
     cond do
       serial_1 == serial_2 ->
-        {:ok, serial_1}
+        serial_1
       true ->
-        {:error, "Serial number mismatch"}
+        :mismatch
     end
   end
 
@@ -72,20 +87,20 @@ defmodule Cereal do
     speak "sh ver | inc Model number"
     IO.puts "Retrieving model number..."
     %{"model" => model_1} = Regex.named_captures(
-      ~r/(?<model>\w+-\w+-\w+)/, listen())
+      @model_regex, listen())
 
     speak "sh inv | inc PID:"
     IO.puts "Retrieving model number..."
     %{"model" => model_2} = Regex.named_captures(
-      ~r/(?<model>\w+-\w+-\w+)/, listen())
+      @model_regex, listen())
 
     IO.puts "Checking model number..."
 
     cond do
       model_1 == model_2 ->
-        {:ok, model_1}
+        model_1
       true ->
-        {:error, "Model number mismatch"}
+        :mismatch
     end
   end
 
@@ -93,26 +108,16 @@ defmodule Cereal do
     speak "sh ver | inc Version"
     IO.puts "Retrieving software version..."
     %{"version" => version} = Regex.named_captures(
-      ~r/Version\s(?<version>\d{2}\.[\w\.)?(?]+)/, listen())
-    cond do
-      version == nil ->
-        {:error, "Version not found"}
-      true ->
-        {:ok, version}
-    end
+      @version_regex, listen())
+    version
   end
 
   def image() do
     speak "sh ver | inc image"
     IO.puts "Retrieving software image..."
     %{"image" => image} = Regex.named_captures(
-      ~r/"flash:\/(?<image>\w+-\w+-\w+\.\d+-\d+\.\w+)/, listen())
-    cond do
-      image == nil ->
-        {:error, "image"}
-      true ->
-        {:ok, image}
-    end
+      @image_regex, listen())
+    image
   end
 
   def sh_inv() do
